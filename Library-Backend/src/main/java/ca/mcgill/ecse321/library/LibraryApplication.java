582123037baf1c78c34acb902bb5f7b14e0faf0b
Abd-El-Aziz Zayed <aziz@macos.local>
2021-10-15 16:21:39 -0400

    first commit

---
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/LibraryApplication.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.library;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world!";
+	}
+
+}
---
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.library;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+class LibraryApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
