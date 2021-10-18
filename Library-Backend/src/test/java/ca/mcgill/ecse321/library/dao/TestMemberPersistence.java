b277c4a08a30702ff351fd2778cd62f0721078b9
saghar sahebi <sagha@Saghar-Macbook-Air.local>
2021-10-18 11:40:24 -0400

    finished persistance test for member

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestMemberPersistence.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestMemberPersistence.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse321.library.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Date;
+
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -7,6 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.library.model.Member;
+import ca.mcgill.ecse321.library.model.Member.MemberStatus;
+import ca.mcgill.ecse321.library.model.Member.MemberType;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -25,5 +34,30 @@ public class TestMemberPersistence {
 
     @Test
     public void testPersistAndLoadCustomer() {
+    Date startDate = Date.valueOf("2021-12-23");
+    	
+    	Member testMember = new Member();
+    	testMember.setUsername("happy");
+    	testMember.setPassword("1234");
+    	testMember.setAddress("199 Rotond");
+    	testMember.setMemberType(MemberType.Local);
+    	testMember.setMemberStatus(MemberStatus.Active);
+    	testMember.setMonthlyFee(0);
+    	testMember.setStartDate(startDate);
+
+		memberRepository.save(testMember);
+		
+		testMember = null;
+
+		testMember = memberRepository.findMemberByUsername("happy");
+		assertNotNull(testMember);
+		
+		assertEquals("happy", testMember.getUsername());
+		assertEquals("1234",testMember.getPassword());
+		assertEquals("199 Rotond",testMember.getAddress());
+		assertEquals(MemberType.Local,testMember.getMemberType());
+		assertEquals(MemberStatus.Active,testMember.getMemberStatus());
+		assertEquals(0,testMember.getMonthlyFee());
+		assertEquals(startDate,testMember.getStartDate());
     }
 }
