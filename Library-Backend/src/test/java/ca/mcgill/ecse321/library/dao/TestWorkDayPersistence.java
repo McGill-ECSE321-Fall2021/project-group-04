3245cf766e7f547b4fe2c25b2766ff43311db772
Jewoo Lee <jwcairo0502@gmail.com>
2021-10-18 19:47:30 -0400

    Fixed bug for Member and Work day persistence

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestWorkDayPersistence.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/dao/TestWorkDayPersistence.java
@@ -13,6 +13,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ca.mcgill.ecse321.library.model.WorkDay;
 import ca.mcgill.ecse321.library.model.WorkDay.DayOfWeek;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestWorkDayPersistence {
@@ -29,7 +32,27 @@ public class TestWorkDayPersistence {
     }
 
     @Test
-    public void testPersistAndLoadWorkDay() {	
-    	
+    public void testPersistAndLoadWorkDay() {
+        WorkDay testWorkday = new WorkDay();
+        Time startTime = Time.valueOf("08:00:00");
+        Time endTime = Time.valueOf("18:00:00");
+
+        //DayOfWeek aDayOfWeek, Time aStartTime, Time aEndTime
+        testWorkday.setDayOfWeek(DayOfWeek.Wednesday);
+        testWorkday.setStartTime(startTime);
+        testWorkday.setEndTime(endTime);
+        testWorkday.setId(12345L);
+
+        workDayRepository.save(testWorkday);
+        testWorkday = workDayRepository.findWorkDayById(testWorkday.getId());
+        //Not null
+        assertNotNull(testWorkday);
+
+        assertEquals(DayOfWeek.Wednesday, testWorkday.getDayOfWeek());
+        assertEquals(Time.valueOf("08:00:00"), testWorkday.getStartTime());
+        assertEquals(Time.valueOf("18:00:00"), testWorkday.getEndTime());
+        //boolean true
+        assertEquals(true, workDayRepository.existsWorkDayById(testWorkday.getId()));
+
     }
 }
