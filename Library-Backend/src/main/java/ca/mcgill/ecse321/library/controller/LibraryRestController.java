8c733bc55549331ca7d3479bd995a86fa7c04224
simo4-4 <59541717+simo4-4@users.noreply.github.com>
2021-11-05 15:23:57 -0400

    Create LibraryRestController.java

---
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/LibraryRestController.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse321.library.controller;
+
+import ca.mcgill.ecse321.library.service.ArchiveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class LibraryRestController {
+
+    //@Autowired
+    //private LibraryService service; //missing LibraryService
+
+}
