b2b340ecbd37b73daafa86234f6eb191d9e61c6d
Jewoo Lee <jwcairo0502@gmail.com>
2021-11-08 20:11:05 -0500

    Login Controller

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/LibraryRestController.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/controller/LibraryRestController.java
@@ -5,7 +5,10 @@ import ca.mcgill.ecse321.library.model.*;
 import ca.mcgill.ecse321.library.service.ArchiveService;
 import ca.mcgill.ecse321.library.service.BookingService;
 import ca.mcgill.ecse321.library.service.UserService;
+import jdk.incubator.vector.VectorOperators;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,6 +34,30 @@ public class LibraryRestController {
         return convertToDto(booking, booking.getUser(), booking.getBookingType());
     }
 
+    @PostMapping(value = {"/login", "/login/"})
+    public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
+        User user = null;
+        try {
+            user = userService.login(username, password);
+        }
+        catch(IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if(user instanceof Member) {
+            return new ResponseEntity<>(convertToDto((Member) user), HttpStatus.OK);
+        }
+
+        if(user instanceof Librarian) {
+            return new ResponseEntity<>(convertToDto((Librarian) user), HttpStatus.OK);
+        }
+
+        if(user instanceof HeadLibrarian) {
+            return new ResponseEntity<>(convertToDto((HeadLibrarian) user), HttpStatus.OK);
+        }
+
+        return null;
+    }
 
 
     private BookingTypeDto convertToDto(BookingType bt){
@@ -47,7 +74,7 @@ public class LibraryRestController {
 
     }
 
-    private UserDto convertToDto(User user){
+    private static UserDto convertToDto(User user){
         return new UserDto(user.getId(),user.getUsername(), user.getPassword(), user.getAddress());
     }
 
