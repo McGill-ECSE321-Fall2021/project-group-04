f203a0ded8b04019a25875f45cd1e5f4949a9cc7
Abd-El-Aziz Zayed <azayed01@gmail.com>
2021-11-10 22:07:36 -0500

    Add @Service tag if missing

--- Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/ReservationService.java
+++ Library-Backend/src/main/java/ca/mcgill/ecse321/library/service/ReservationService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
