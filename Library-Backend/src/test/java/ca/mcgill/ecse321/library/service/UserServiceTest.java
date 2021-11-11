db54f7ce6a440c74613a034ed99842ed143acf9d
Jewoo Lee <jwcairo0502@gmail.com>
2021-11-11 02:17:53 -0500

    Fixed UserServiceTest

--- Library-Backend/src/test/java/ca/mcgill/ecse321/library/service/UserServiceTest.java
+++ Library-Backend/src/test/java/ca/mcgill/ecse321/library/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import ca.mcgill.ecse321.library.dao.MemberRepository;
 import ca.mcgill.ecse321.library.model.Member;
+import ca.mcgill.ecse321.library.model.User;
 import net.bytebuddy.pool.TypePool;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +27,9 @@ public class UserServiceTest {
     private MemberRepository memberRepository;
 
     @InjectMocks
+    private UserService userService;
+
+    @InjectMocks
     private MemberService memberService;
 
     private static final String MEMBER_USERNAME = "TestMember";
@@ -42,110 +46,4 @@ public class UserServiceTest {
 
     }
 
-    @Test
-    public void testSignupMember() {
-
-    }
-
-    @Test
-    public void testSignupMemberExistingUsername() {
-
-    }
-
-    @Test
-    public void testUpdateMemberPassword() {
-        assertEquals(0, memberService.getAllMembers().size());
-        Member member = null;
-
-        try {
-            member = memberService.changeMemberPassword(MEMBER_USERNAME, "Newpassword123");
-        }
-        catch(IllegalArgumentException e) {
-            fail();
-        }
-        assertNotNull(member);
-        assertEquals("Newpassword123", member.getPassword());
-        assertEquals(MEMBER_USERNAME, member.getUsername());
-    }
-
-    @Test
-    public void testUpdateMemberPasswordInvalidUpperCase() {
-        assertEquals(0, memberService.getAllMembers().size());
-        Member member = null;
-        String error = null;
-
-        try {
-            member = memberService.changeMemberPassword(MEMBER_USERNAME, "invalidpassword123");
-        }
-        catch(IllegalArgumentException e) {
-            error = e.getMessage();
-        }
-        assertNull(member);
-        assertEquals(error, "Invalid Password. Password must contain at least one uppercase character.");
-    }
-
-    @Test
-    public void testUpdateMemberPasswordEmptyPassword() {
-        assertEquals(0, memberService.getAllMembers().size());
-        Member member = null;
-        String error = null;
-
-        try {
-            member = memberService.changeMemberPassword(MEMBER_USERNAME, null);
-        }
-        catch(IllegalArgumentException e) {
-            error = e.getMessage();
-        }
-        assertNull(member);
-        assertEquals(error, "Invalid Password. New password cannot be empty.");
-    }
-
-    @Test
-    public void testDeleteMember() {
-        boolean deleted = false;
-        try {
-            deleted = memberService.deleteMember(MEMBER_USERNAME);
-        }
-        catch(IllegalArgumentException e) {
-            fail();
-        }
-        assertTrue(deleted);
-    }
-
-    @Test
-    public void testDeleteMemberNotFound() {
-        String error = null;
-        try {
-            memberService.deleteMember("Non-existing Username");
-        }
-        catch(IllegalArgumentException e) {
-            error = e.getMessage();
-        }
-        assertEquals(error, "Member is not found.");
-    }
-
-    @Test
-    public void testInvalidPasswordUpperCase() {
-
-    }
-
-    @Test
-    public void testInvalidPasswordLowerCase() {
-
-    }
-
-    @Test
-    public void testInvalidPasswordNumber() {
-
-    }
-
-    @Test
-    public void testInvalidPasswordLengthUnder8() {
-
-    }
-
-    @Test
-    public void testInvalidPasswordLengthOver20() {
-
-    }
 }
