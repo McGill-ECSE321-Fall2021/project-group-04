9f9425dd90f1a0303a8345698083546d22714014
AlexandreSaroufim <77616131+AlexandreSaroufim@users.noreply.github.com>
2021-12-03 22:11:45 -0500

    Backend x Android

--- Library-Android/app/src/main/java/ca/mcgill/ecse321/library_android/MainActivity.java
+++ Library-Android/app/src/main/java/ca/mcgill/ecse321/library_android/MainActivity.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.library_android;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,11 +20,20 @@ import ca.mcgill.ecse321.library_android.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private String error = null;
+    private String userName= null;
+    private String userType = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,4 +85,88 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
+//    private void refreshErrorMessage() {
+//        // set the error message
+//        TextView tvError = (TextView) findViewById(R.id.error);
+//        tvError.setText(error);
+//
+//        if (error == null || error.length() == 0) {
+//            tvError.setVisibility(View.GONE);
+//        } else {
+//            tvError.setVisibility(View.VISIBLE);
+//        }
+//
+//    }
+
+    public void login(View view){
+        final EditText username = findViewById(R.id.Username);
+        final EditText password = findViewById(R.id.Password);
+        RequestParams rp = new RequestParams();
+        rp.put("username", username.getText());
+        rp.put("password", password.getText());
+
+        HttpUtils.post("login", rp, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    System.out.println("tada");
+                    JSONObject serverResp = new JSONObject(response.toString());
+                    userName = serverResp.getString("username");
+                    userType = serverResp.getString("userType");
+                    if(!userType.equals("member")){
+                        //search a pop up to say access denied
+                        new SweetAlertDialog(MainActivity.this).setTitleText("Access Denied").show();
+                        userName = null;
+                        userType = null;
+                    }
+                    else{
+                        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
+                        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+                        bottomNav.setVisibility(View.VISIBLE);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                try {
+                    new SweetAlertDialog(MainActivity.this)
+                            .setTitleText(errorResponse.get("message").toString())
+                            .show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, String errorMessage, Throwable throwable) {
+                try {
+                    new SweetAlertDialog(MainActivity.this)
+                            .setTitleText(errorMessage)
+                            .show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+//        public void goToSignup(View view){
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    new Signup()).commit();
+//        }
+
+    }
+
+
+
+
+
+
+
 }
---
+++ Library-Android/app/src/main/java/ca/mcgill/ecse321/library_android/SignUp.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse321.library_android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SignUp {
+
+
+//    public View onCreateView(
+//            LayoutInflater inflater, ViewGroup container,
+//            Bundle savedInstanceState
+//    ) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.signup, container, false);
+//    }
+
+}
