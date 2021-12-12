49c1a124a5240864d3373638b6d9d7560d072185
Abd-El-Aziz Zayed <azayed01@gmail.com>
2021-12-12 00:42:08 -0500

    dep

--- Library-Frontend/src/views/LibrarianScheduler.vue
+++ Library-Frontend/src/views/LibrarianScheduler.vue
@@ -30,6 +30,7 @@
                 <div class="row align-items-center">
                   <div class="col-8">
                     <h3 class="mb-0">Librarians</h3>
+
                   </div>
                 </div>
               </div>
